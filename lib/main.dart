ad8a52bd92f54a221dfa61d6df6fa4dc6c934479
wk <doubleuk.lee@gmail.com>
2020-04-16 12:30:12 +0900

    complete torch / http com / camera / tap position

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 import 'package:touchless_kiosk_mobile/camera_view.dart';
 import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'package:torch/torch.dart';
+
 
 
 List<CameraDescription> cameras;
+final gServerIp = 'http://192.168.0.4:5000/';
+String positionX, positionY;
 
 Future<Null> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   cameras = await availableCameras();
   runApp(new MyApp());
+}
+
+Future<String> postReply() async {
+  var addr = gServerIp + 'post';
+  var response = await http.post(addr, body: {'dx': positionX, 'dy': positionY}); 
+
+  // 200 ok. 정상 동작임을 알려준다.
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    print("error");
+  }
+    
+  
+  // 데이터 수신을 하지 못했다고 예외를 일으키는 것은 틀렸다.
+  // 여기서는 코드를 간단하게 처리하기 위해.
+  throw Exception('데이터 수신 실패!');
 }
 
 
@@ -36,7 +58,6 @@ class _TouchlessKioskAppPageState extends State<TouchlessKioskAppPage> {
 
   var message = "sample";
 
-
   @override
   void initState() {
     super.initState();
@@ -57,7 +78,19 @@ class _TouchlessKioskAppPageState extends State<TouchlessKioskAppPage> {
             onTap: () {
             },
             onTapDown: (TapDownDetails detail) {
-              print(detail.globalPosition.dx.toString() + " / " + (detail.globalPosition.dy - 150).toString());
+              positionX = detail.globalPosition.dx.toStringAsFixed(0);
+              positionY = (detail.globalPosition.dy - 150).toStringAsFixed(0);
+              setState(() {
+                message = "X : " + positionX + " / Y : " + positionY;
+              });
+              Future.delayed(Duration(milliseconds: 40), () => Torch.flash(Duration(milliseconds: 20)));
+              Torch.flash(Duration(milliseconds: 20));
+              try {
+                postReply()
+                .then((recvd) => print(recvd));
+              } catch (e,s) {
+                print(s);
+              }
             },
 
             child: Container(
@@ -67,7 +100,9 @@ class _TouchlessKioskAppPageState extends State<TouchlessKioskAppPage> {
             ),
           ),
           Text(
-            message
+            message,
+            textAlign: TextAlign.center,
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.redAccent, fontSize: 20),
           ),
         ]
       ),
